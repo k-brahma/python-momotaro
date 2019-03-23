4705a85aec85b2236e1beec7428f78d935158221
k-brahma <k.brahma.nirvana@gmail.com>
2019-03-23 13:34:29 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Twitterで桃太郎が流行っているので書いてみた。
-https://twitter.com/kanjizaibosatsu/status/1108525993528942593
-https://twitter.com/search?q=%E6%A1%83%E5%A4%AA%E9%83%8E&src=typd&lang=ja
+
+娘「桃太郎読んで」
+母「昔々」
+娘「Pythonぽく」
+「p = Party(master='momotaro', target='oni', kibi=7)
+p.addmember(['inu', 'saru', 'kiji', ])
+print(p.kibi)  # 4
--- momo.py
+++ momo.py
@@ -1,14 +1,14 @@
 from creature import Human, Animal, Oni
 
 
-class Team:
+class Party:
     member = []
 
-    def __init__(self, target, boss, kibi):
+    def __init__(self, master, target, kibi):
         self.target = Oni(target)
-        boss = Human(boss)
-        boss.IsBoss = True
-        self.member.append(boss)
+        master = Human(master)
+        master.IsMaster = True
+        self.member.append(master)
         self.kibi = kibi
 
     def addmember(self, animals):
@@ -17,20 +17,22 @@ class Team:
             self.kibi += -1
 
 
-team = Team(target='oni', boss='momotaro', kibi=7)
-team.addmember(['inu', 'saru', 'kiji', ])
-print('target:' + team.target.typename)
+p = Party(master='momotaro', target='oni', kibi=7)
+p.addmember(['inu', 'saru', 'kiji', ])
+print(p.kibi)  # 4
 
-cnt = 1
-member = ''
-for s in team.member:
-    member = s.gettype()
-    if hasattr(s, 'IsBoss'):
-        if getattr(s, 'IsBoss'):
-            member += ' [*]'
-            boss = s.gettype()
-
-    print('member' + str(cnt) + ':' + member)
-    cnt += 1
-
-print('kibidango remained:' + str(team.kibi))  # 4
+# print('target:' + p.target.typename)
+#
+# cnt = 1
+# member = ''
+# for s in p.member:
+#     member = s.gettype()
+#     if hasattr(s, 'IsBoss'):
+#         if getattr(s, 'IsBoss'):
+#             member += ' [*]'
+#             boss = s.gettype()
+#
+#     print('member' + str(cnt) + ':' + member)
+#     cnt += 1
+#
+# print('kibidango remained:' + str(p.kibi))  # 4
